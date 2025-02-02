b91fa744067d7840b6b627f24884ebafb2032089
Omar Sadek <sadekm.omar@gmail.com>
2025-02-02 02:51:34 +0200

    fixed grid

--- apps/web/src/layouts/GridLayout/GridLayout.jsx
+++ apps/web/src/layouts/GridLayout/GridLayout.jsx
@@ -1,40 +1,39 @@
-import './GridLayout.css'
+import "./GridLayout.css";
 
-import { ItemCard } from '../../components/ItemCard/ItemCard';
-import { GridPlaceholder } from './GridPlaceholder';
+import { ItemCard } from "../../components/ItemCard/ItemCard";
+import { GridPlaceholder } from "./GridPlaceholder";
 
 export function GridLayout({ products, emptyState }) {
-    return <>
-        {
-            (products && products.length != 0) ?
-            <div className="wrapper">
-                <div className="grid">
-                    {
-                        products.map((product, index) => (
-                            <ItemCard
-                                key={index}
-                                id={product['id']}
-                                name={product['name']}
-                                price={product['price']}
-                                brand={product['brand']}
-                                date={product['date']}
-                                description={product['description']}
-                                link={product['link']}
-                                images={product['images']}
-                                sizes={product['sizes']}
-                                gender={product['gender']}
-                                material={product['material']}
-                                category={product['category']}
-                                color={product['color']}
-                                colors={product['colors']}
-                            />
-                        ))
-                    }
-                </div>
-            </div> :
-            <GridPlaceholder />
-        }
-
-    </>;
+  return (
+    <>
+      {products && products.length != 0 ? (
+        <div className="wrapper">
+          <div className="grid">
+            {products.map((product, index) => (
+              <ItemCard
+                key={index}
+                id={product["id"]}
+                name={product["name"]}
+                price={product["price"]}
+                brand={product["brand"]}
+                date={product["date"]}
+                description={product["description"]}
+                link={product["link"]}
+                src={product["src"]}
+                srcSet={product["srcSet"]}
+                sizes={product["sizes"]}
+                gender={product["gender"]}
+                material={product["material"]}
+                category={product["category"]}
+                color={product["color"]}
+                colors={product["colors"]}
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <GridPlaceholder />
+      )}
+    </>
+  );
 }
-
