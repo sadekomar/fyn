b88e5e20bb80146087bb15b2ab25766dda105ed4
Omar Sadek <sadekm.omar@gmail.com>
2025-05-11 17:34:51 +0300

    changed all brands api

--- apps/api/src/handlers/get-all-brands.handler.ts
+++ apps/api/src/handlers/get-all-brands.handler.ts
@@ -1,11 +1,25 @@
+import { Brand } from "@prisma/client";
 import prisma from "../lib/prisma";
 import { handleExceptions } from "../lib/utils";
 import { Request, Response } from "express";
 
 export const getAllBrands = handleExceptions(
   async (req: Request, res: Response) => {
-    const brands = await prisma.brand.findMany();
+    const brands = await prisma.brand.findMany({
+      orderBy: {
+        name: "asc",
+      },
+    });
 
-    return res.json(brands);
+    const brandsByLetter = brands.reduce((acc, brand) => {
+      const letter = brand.name[0].toUpperCase();
+      if (!acc[letter]) {
+        acc[letter] = [];
+      }
+      acc[letter].push(brand);
+      return acc;
+    }, {} as Record<string, Brand[]>);
+
+    return res.json(brandsByLetter);
   }
 );
