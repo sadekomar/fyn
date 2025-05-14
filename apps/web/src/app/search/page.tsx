79fbe78064129ed23c6e751219f55a71ce343aea
Omar Sadek <sadekm.omar@gmail.com>
2025-05-14 03:13:32 +0300

    search page now works

--- apps/web/src/app/search/page.tsx
+++ apps/web/src/app/search/page.tsx
@@ -4,12 +4,11 @@ import React, { useEffect, useRef, useState } from "react";
 import { Flex } from "@radix-ui/themes";
 import { ClockIcon, X, Search } from "lucide-react";
 
-import { IPAddress } from "../../data/IPAddress";
 import { autofillSuggestionsList } from "../../data/autofillSuggestions";
 
 import { GridLayout } from "../../layouts/GridLayout/GridLayout";
 
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, usePathname } from "next/navigation";
 import "./SearchPage.css";
 import { PaginationControl } from "@/components/Pagination/PaginationControl";
 
@@ -18,15 +17,14 @@ import { ColorPills } from "../(home)/(ColorPills)/ColorPills";
 import { ItemCardsI, MetadataI } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import { HttpMethods, httpService } from "@/queries/http.service";
-import { ControlledSearchComponent } from "./SearchComponent";
 
 export default function SearchPage() {
-  const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   const queryString = searchParams.toString();
   const queryStringArray = Array.from(searchParams.entries());
+  console.log(queryStringArray);
 
   const { data: products } = useQuery({
     queryKey: ["/search", ...queryStringArray],
@@ -36,6 +34,8 @@ export default function SearchPage() {
         isResponseJson: true,
       }),
   });
+
+  console.log(products);
 
   const { data: metadata } = useQuery({
     queryKey: ["/metadata", ...queryStringArray],
@@ -50,76 +50,15 @@ export default function SearchPage() {
       ),
   });
 
-  // let [products, setProducts] = useState<ItemCardsI[]>([]);
-  // let [metadata, setMetadata] = useState<MetadataI>();
   let searchFieldRef = useRef<HTMLInputElement>(null);
   let autofillRef = useRef<HTMLDivElement>(null);
-  const [searchField, setSearchField] = useState<string>("");
   let [searchHistory, setSearchHistory] = useState<string[]>([]);
   let [autofill, setAutofill] = useState<string[]>([]);
 
-  // useEffect(() => {
-  //   if (
-  //     searchParams.toString() != "" &&
-  //     searchParams.get("query") != "" &&
-  //     searchParams.get("query") != " "
-  //   ) {
-  //     setProducts(null);
-  //     searchFieldRef.current.value = searchParams.get("query");
-  //     fetch(`${IPAddress}/search?${searchParams.toString()}`)
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         setProducts(data || null);
-  //       })
-  //       .catch((reason) => console.log(reason));
-
-  //     fetch(`${IPAddress}/metadata?${searchParams.toString()}`)
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         setMetadata(data);
-  //         setNumberOfItems(data["item_count"] || 0);
-  //       })
-  //       .catch((reason) => console.log(reason));
-  //   } else {
-  //     searchFieldRef.current.focus();
-  //   }
-  // }, [
-  //   searchParams.get("brand"),
-  //   searchParams.get("gender"),
-  //   searchParams.get("color"),
-  //   searchParams.get("category"),
-  //   searchParams.get("material"),
-  //   searchParams.get("sort_by"),
-  //   searchParams.get("availability"),
-  //   searchParams.get("page"),
-  // ]);
-
-  // okay how does search work?
-  // just like filters i want search to do that as well
-  // on mount if a search param exists then fetch the data
-  //
-
   // Fetching data
   function search(query: string) {
-    if (query != "" && query != " ") {
-      // fetch(`${IPAddress}/search?query=${query}`)
-      //   .then((response) => response.json())
-      //   .then((data) => {
-      //     setProducts(data || null);
-      //   })
-      //   .catch((reason) => console.log(reason));
-
-      // fetch(`${IPAddress}/metadata?query=${query}`)
-      //   .then((response) => response.json())
-      //   .then((data) => {
-      //     setMetadata(data);
-      //     setNumberOfItems(data["item_count"] || 0);
-      //   })
-      //   .catch((reason) => console.log(reason));
-
-      addToSearchHistory(query);
-      router.push(pathname + `?query=${query}`);
-    }
+    addToSearchHistory(query);
+    window.history.pushState(null, "", pathname + `?search=${query}`);
   }
 
   // Autofill
@@ -162,7 +101,10 @@ export default function SearchPage() {
     );
     recentlySearched.reverse();
     setSearchHistory(recentlySearched);
-  }, []);
+    if (searchFieldRef.current) {
+      searchFieldRef.current.value = searchParams.get("search") || "";
+    }
+  }, [searchParams]);
 
   function addToSearchHistory(searchString: string) {
     if (searchString != "" && searchString != " ") {
@@ -215,10 +157,9 @@ export default function SearchPage() {
   return (
     <>
       <Flex direction={"column"}>
-        <ControlledSearchComponent />
         <div className="search-and-autofill">
           <div
-            className={`searchbar-wrapper ${!searchParams.get("query") ? "startup-searchbar-wrapper" : ""}`}
+            className={`searchbar-wrapper ${!searchParams.get("search") ? "startup-searchbar-wrapper" : ""}`}
           >
             <div className="searchbar">
               <div className="magnifying-glass">
@@ -231,8 +172,10 @@ export default function SearchPage() {
                   console.log("submitted");
 
                   if (searchFieldRef.current) {
-                    router.push(
-                      pathname + `?query=${searchFieldRef.current.value}`,
+                    window.history.pushState(
+                      null,
+                      "",
+                      pathname + `?search=${searchFieldRef.current.value}`,
                     );
 
                     search(searchFieldRef.current.value);
@@ -248,12 +191,8 @@ export default function SearchPage() {
                   placeholder="Search"
                   name="search"
                   ref={searchFieldRef}
-                  value={searchField}
                   type="search"
-                  onChange={(e) => {
-                    setSearchField(e.target.value);
-                    // generateAutofillSuggestions();
-                  }}
+                  onChange={generateAutofillSuggestions}
                   onFocus={() => {
                     autofillRef.current?.classList.remove("search-blurred");
                   }}
@@ -281,7 +220,7 @@ export default function SearchPage() {
           </div>
           <div
             ref={autofillRef}
-            className={`autofill-wrapper search-blurred ${!searchParams.get("query") ? "autofill-wrapper-initial" : ""}`}
+            className={`autofill-wrapper search-blurred ${!searchParams.get("search") ? "autofill-wrapper-initial" : ""}`}
           >
             {searchHistory &&
               searchHistory.slice(0, 12).map((suggestion, index) => (
@@ -292,7 +231,11 @@ export default function SearchPage() {
                       if (searchFieldRef.current) {
                         searchFieldRef.current.value = suggestion;
                       }
-                      router.push(pathname + `?query=${suggestion}`);
+                      window.history.pushState(
+                        null,
+                        "",
+                        pathname + `?search=${suggestion}`,
+                      );
 
                       search(suggestion);
                       generateAutofillSuggestions();
@@ -319,7 +262,11 @@ export default function SearchPage() {
                     if (searchFieldRef.current) {
                       searchFieldRef.current.value = suggestion;
                     }
-                    router.push(pathname + `?query=${suggestion}`);
+                    window.history.pushState(
+                      null,
+                      "",
+                      pathname + `?search=${suggestion}`,
+                    );
 
                     search(suggestion);
                     generateAutofillSuggestions();
@@ -332,7 +279,7 @@ export default function SearchPage() {
           </div>
         </div>
 
-        {searchParams.get("query") && (
+        {searchParams.get("search") && (
           <>
             <ColorPills metadata={metadata} />
             <FiltersAndCount metadata={metadata} />
