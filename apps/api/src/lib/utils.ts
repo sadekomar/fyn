809e0d2b6be249391789243e1d24cf400aa54fd3
Omar Sadek <sadekm.omar@gmail.com>
2025-05-20 20:00:48 +0300

    error logging

--- apps/api/src/lib/utils.ts
+++ apps/api/src/lib/utils.ts
@@ -6,7 +6,8 @@ export const handleExceptions =
   (fn: HandlerFunction) => async (req: Request, res: Response) => {
     try {
       await fn(req, res);
-    } catch {
+    } catch (error) {
+      console.error(error);
       res.status(500).json({ error: "Failed to fetch items" });
     }
   };
